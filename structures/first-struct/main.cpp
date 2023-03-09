5c8d73eb451323bd467cf22a81bbd3c16124af42
ahmedhossam <hoss4614@gmail.com>
2023-03-09 21:54:30 +0200

    finish structures example 1

--- structures/first-struct/main.cpp
+++ structures/first-struct/main.cpp
@@ -1,23 +1,61 @@
 #include <iostream>
 using namespace std;
 
-struct part {
-  int modelnumber; //ID number of widget
-  int partnumber; // ID number of widget part
-  float cost; //cost of part
-};
+struct card
+	{
+	int number;
+	int suit;
+	};
 
-int main() {
-  part part1; // instance of the structure " part "
+const int clubs = 0;
+const int diamonds = 1;
+const int hearts = 2;
+const int spades = 3;
 
-  part1.modelnumber = 6244;
-  part1.partnumber = 373;
-  part1.cost =  217.55F;
+const int jack = 11;
+const int queen = 12;
+const int king = 13;
+const int ace = 14;
 
-  cout << "Model : " << part1.modelnumber << endl;
-  cout << "Part : " << part1.partnumber << endl;
-  cout << "Costs = " << part1.cost << endl;
-  cout << "First c++ line from 'pure' vim code editor" << endl;
+int main () {
+	card temp, chosen, prize;
+	int position;
 
-  return 0;  
+	card card1 = { 7 , clubs };
+	cout << "Card 1 is the jack of clubs\n";
+
+	card card2 = { jack, hearts };
+	cout << "Card 2 is the jack of hearts\n";
+
+	card card3 = { ace , spades };
+	cout << "Card 3 is the ace of spades\n";
+
+	prize = card3; // copy this card , to remember it
+
+	cout << "I'm swapping card 1 and card 3\n";
+	temp = card3; card3 = card1; card1 = temp; // swapping
+
+	cout << "I'm swapping card 2 and card 3\n";
+	temp = card3; card3 = card2; card2 = temp; // swapping
+
+	cout << "I'm swapping card 1 and card 2\n";
+	temp = card2; card2 = card1; card1 = temp; // swapping
+
+	cout << "Now, where (1, 2 , or 3) is the ace of shades ? : ";
+	cin >> position;
+
+	switch (position)
+	{
+	case 1: chosen = card1; break;
+	case 2: chosen = card2; break;
+	case 3: chosen = card3; break;
+	}
+
+	if ( chosen.number == prize.number && chosen.suit == prize.suit)
+		cout << "That's right ! You win!!\n";
+	else
+		cout << "Sorry. You Lost\n";
+
+
+	return 0;
 }
